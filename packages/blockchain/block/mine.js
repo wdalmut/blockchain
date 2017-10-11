cb48faac267edda08f865d47c990fc2916b4766a
Walter Dal Mut <walter.dalmut@gmail.com>
2017-10-11 20:11:12 +0200

    Added abort mechanism during the mining operation

--- packages/blockchain/block/mine.js
+++ packages/blockchain/block/mine.js
@@ -1,22 +1,35 @@
 const mine = (hash, complexity, block, callback) => {
   let verify = 0x0;
 
+  let abortId = 0x0;
+
+  const abort = () => {
+    clearImmediate(abortId);
+    callback(new Error("abort"), undefined);
+  };
+
   let doHash = (hash, complexity, block) => {
     block = Object.assign({}, block, {nonce: ++block.nonce});
     verify = hash(JSON.stringify(block));
 
     if (verify.slice(0, complexity) != Array.apply(null, Array(complexity)).map(() => 0).join('')) {
-      return setImmediate(doHash, hash, complexity, block);
+      abortId = setImmediate(doHash, hash, complexity, block);
     }
 
     return callback(undefined, Object.assign({}, block, {hash: verify}));
   };
 
-  return setImmediate(doHash, hash, complexity, block);
+  abortId = setImmediate(doHash, hash, complexity, block);
+
+  return abort;
 };
 
 const signBlock = (mySignature, hash, complexity, block, callback) => {
-  mine(hash, complexity, block, (err, block) => {
+  return mine(hash, complexity, block, (err, block) => {
+    if (err) {
+      return callback(err, undefined);
+    }
+
     block = Object.assign({}, block, {signature: mySignature(JSON.stringify(block))});
 
     callback(undefined, block);
--- packages/blockchain/spec/block/mine-spec.js
+++ packages/blockchain/spec/block/mine-spec.js
@@ -3,16 +3,40 @@ const {signBlock} = require('../../block/mine');
 
 describe("Mining", () => {
   it("should sign a block asynchrously", (done) => {
-    let block = signBlock(
+    signBlock(
       () => "sign",
       () => "00000000000",
       2,
       createBlock("test", 1, {}),
       (err, block) => {
+        if (err) {
+          fail();
+        }
+
         expect(block.hash).toEqual("00000000000");
         expect(block.signature).toEqual("sign");
         done();
       }
     );
+  });
+
+  it("should abort current mining", (done) => {
+    const {hash} = require('crypto-helpers');
+
+    let abort = signBlock(
+      () => "sign",
+      hash,
+      10,
+      createBlock("test", 1, {}),
+      (err, block) => {
+        if (!err) {
+          fail();
+        }
+
+        return done();
+      }
+    );
+
+    abort();
   });
 });
