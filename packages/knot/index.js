7e92d395c1f31551ad0f0d89c39ed45fa6e83dea
Walter Dal Mut <walter.dalmut@gmail.com>
2017-10-15 20:56:34 +0200

    Fixes wrong mined block operations order

--- packages/knot/index.js
+++ packages/knot/index.js
@@ -7,7 +7,6 @@ const {hash} = require('crypto-helpers'),
   chainFrom = require('./chain-from'),
   chainPartial = require('./chain-partial')
 ;
-
 module.exports = (message) => {
   message.stream.pipe(blackhole()); // line tapping
 
@@ -24,20 +23,15 @@ module.exports = (message) => {
   message.stream.on('block.mined', blockMined(message));
   message.stream.on('chain.from', chainFrom(message));
   message.stream.on('chain.partial', chainPartial(message));
-  message.stream.on('chain.update', (chain) => {
-    console.log(JSON.stringify(chain, null, 2));
-  });
 
   miner.start((transactions, done) => {
     let abort = message.sign(message.createBlock(message.chain, new Date().getTime(), transactions), (err, block) => {
-      if (err) {
-        return;
-      }
-
       message.stream.removeListener('block.mined', abort);
       message.stream.removeListener('chain.update', abort);
 
-      message.stream.emit('block.mined', block);
+      if (!err) {
+        message.stream.emit('block.mined', block);
+      }
 
       done();
     });
