9758167379b78a8818a099c78e20a8087b2355a2
Walter Dal Mut <walter.dalmut@gmail.com>
2017-10-15 11:11:40 +0200

    Split base index to identify responsibilities

---
+++ packages/knot/block-mined.js
@@ -0,0 +1,28 @@
+const merkle = require('merkle-tree'),
+  {createBlockMinedMessage} = require('network/message'),
+  transaction = require('blockchain/transaction')
+;
+
+module.exports = (message) => (block) => {
+  let transactions = block.transactions;
+
+  if(!merkle.validate(hash, transactions)) {
+    return message.stream.emit('error.block.mined', block);
+  }
+
+  if(!(transaction.verifyAllTransactions(merkle.clean(transactions)))) {
+    return message.stream.emit('error.block.mined', block);
+  }
+
+  let chain = message.chain.concat([block]);
+
+  if (!message.isBlockchainValid(chain)) {
+    return message.stream.emit('error.block.mined', block);
+  }
+
+  message.chain = chain;
+
+  console.log(JSON.stringify(message.chain, null, 2));
+
+  message.stream.add(createBlockMinedMessage(block));
+};
--- packages/knot/index.js
+++ packages/knot/index.js
@@ -2,10 +2,8 @@ const {hash} = require('crypto-helpers'),
   scheduler = require('scheduler'),
   network = require('network'),
   {connectPeer} = require('network/peer'),
-  {createBlockMinedMessage} = require('network/message'),
   blackhole = require('network/stream/blackhole'),
-  merkle = require('merkle-tree'),
-  transaction = require('blockchain/transaction')
+  blockMined = require('./block-mined')
 ;
 
 module.exports = (message) => {
@@ -22,32 +20,7 @@ module.exports = (message) => {
 
   message.stream.on('transaction.append', miner.add);
 
-  message.stream.on('block.mined', (block) => {
-    let transactions = block.transactions;
-
-    if(!merkle.validate(hash, transactions)) {
-      console.log("Invalid blockchain with block (merkle-tree)", block);
-      return;
-    }
-
-    if(!(transaction.verifyAllTransactions(merkle.clean(transactions)))) {
-      console.log("invalid blockchain with block (transaction verify)", block);
-      return;
-    }
-
-    let chain = message.chain.concat([block]);
-
-    if (!message.isBlockchainValid(chain)) {
-      console.log("invalid blockchain with block", block);
-      return;
-    }
-
-    message.chain = chain;
-
-    console.log(JSON.stringify(message.chain, null, 2));
-
-    message.stream.add(createBlockMinedMessage(block));
-  });
+  message.stream.on('block.mined', blockMined(message));
 
   miner.start((transactions, done) => {
     let abort = message.sign(message.createBlock(message.chain, new Date().getTime(), transactions), (err, block) => {
