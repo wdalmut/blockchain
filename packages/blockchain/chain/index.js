8118c89cbca0fbc7d9372b82c3626c932c2707df
Walter Dal Mut <walter.dalmut@gmail.com>
2017-10-15 10:19:00 +0200

    Added chain split up method to get other chain pieces

--- packages/blockchain/chain/index.js
+++ packages/blockchain/chain/index.js
@@ -2,6 +2,19 @@ const from = (chain, index) => {
   return chain.slice(index-1);
 };
 
+const apart = (chain) => {
+  const middle = (memo, remains) => {
+    while (remains.length >= 1) {
+      return middle(memo.concat(remains.slice(-1)), remains.slice(0, remains.length/2));
+    }
+
+    return memo;
+  }
+
+  return middle([], chain);
+};
+
 module.exports = {
   from,
+  apart,
 };
--- packages/blockchain/spec/chain/index-spec.js
+++ packages/blockchain/spec/chain/index-spec.js
@@ -1,9 +1,27 @@
-const { from } = require('../../chain');
+const { from, apart } = require('../../chain');
 
 describe("Chain", () => {
   it("should return from a index", () => {
     let chain = [1,2,3,4,5,6,7,8,9,];
 
     expect(from(chain, 5)).toEqual([5,6,7,8,9]);
+  });
+
+  it("should split my chain down piece by piece", () => {
+   let chain = [1,2,3,4,5,6,7,8,9,];
+
+    expect(apart(chain)).toEqual([9,4,2,1]);
+  });
+
+  it("should split my chain down piece by piece with empty chain", () => {
+   let chain = [];
+
+    expect(apart(chain)).toEqual([]);
+  });
+
+  it("should split my chain down piece by piece with one block", () => {
+   let chain = [1];
+
+    expect(apart(chain)).toEqual([1]);
   });
 });
